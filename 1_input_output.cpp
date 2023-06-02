b79e681c7283bbfecc689b3d387ba265a7975a66
Sanjeev <109661686+softlabxsanjeev1@users.noreply.github.com>
2023-06-02 22:24:04 +0530

    Add files via upload

---
+++ 1_input_output.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include<conio.h>             
+using namespace std;
+int main(){
+    int x;
+    int y;
+    int sum;
+    cout << "Type a number : ";
+    cin >> x;
+    cout << "Type a number : ";
+    cin >> y;
+    sum=x+y;
+    cout << "your no is = " << sum;
+    getch();
+}
+
+
